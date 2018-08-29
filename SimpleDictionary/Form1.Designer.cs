a25a836c7d8c7d2a67c1ac645d47a4b673b48d6a
Minh Phong <dominhphong306@gmail.com>
2018-08-29 21:33:41 +0700

    Add project files.

---
+++ SimpleDictionary/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace SimpleDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ptbMeaning = new System.Windows.Forms.PictureBox();
+            this.rtbMeaning = new System.Windows.Forms.RichTextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.lsbWord = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbAddMeaning = new System.Windows.Forms.RichTextBox();
+            this.txtAddPicturePath = new System.Windows.Forms.TextBox();
+            this.txtAddWord = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAddBrowsePicture = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.btnResetDefault = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMeaning)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Cooper Std Black", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(575, 29);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(446, 52);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Simple Dictionary";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ptbMeaning);
+            this.groupBox1.Controls.Add(this.rtbMeaning);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnTranslate);
+            this.groupBox1.Controls.Add(this.lsbWord);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(14, 234);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(931, 505);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dictionary";
+            // 
+            // ptbMeaning
+            // 
+            this.ptbMeaning.Image = global::SimpleDictionary.Properties.Resources._default;
+            this.ptbMeaning.Location = new System.Drawing.Point(526, 318);
+            this.ptbMeaning.Name = "ptbMeaning";
+            this.ptbMeaning.Size = new System.Drawing.Size(374, 164);
+            this.ptbMeaning.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptbMeaning.TabIndex = 3;
+            this.ptbMeaning.TabStop = false;
+            // 
+            // rtbMeaning
+            // 
+            this.rtbMeaning.Location = new System.Drawing.Point(521, 66);
+            this.rtbMeaning.Name = "rtbMeaning";
+            this.rtbMeaning.Size = new System.Drawing.Size(380, 173);
+            this.rtbMeaning.TabIndex = 2;
+            this.rtbMeaning.Text = "";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(341, 221);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(157, 45);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(341, 124);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(157, 45);
+            this.btnTranslate.TabIndex = 1;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // lsbWord
+            // 
+            this.lsbWord.FormattingEnabled = true;
+            this.lsbWord.ItemHeight = 29;
+            this.lsbWord.Location = new System.Drawing.Point(8, 72);
+            this.lsbWord.Name = "lsbWord";
+            this.lsbWord.Size = new System.Drawing.Size(307, 410);
+            this.lsbWord.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(558, 260);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 32);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Picture";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(558, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Meaning";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(372, 32);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 32);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Action";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Words";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbAddMeaning);
+            this.groupBox2.Controls.Add(this.txtAddPicturePath);
+            this.groupBox2.Controls.Add(this.txtAddWord);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnAddBrowsePicture);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(955, 234);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Size = new System.Drawing.Size(519, 505);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add new word";
+            // 
+            // rtbAddMeaning
+            // 
+            this.rtbAddMeaning.Location = new System.Drawing.Point(148, 124);
+            this.rtbAddMeaning.Name = "rtbAddMeaning";
+            this.rtbAddMeaning.Size = new System.Drawing.Size(342, 130);
+            this.rtbAddMeaning.TabIndex = 2;
+            this.rtbAddMeaning.Text = "";
+            // 
+            // txtAddPicturePath
+            // 
+            this.txtAddPicturePath.Enabled = false;
+            this.txtAddPicturePath.Location = new System.Drawing.Point(153, 291);
+            this.txtAddPicturePath.Name = "txtAddPicturePath";
+            this.txtAddPicturePath.Size = new System.Drawing.Size(266, 34);
+            this.txtAddPicturePath.TabIndex = 2;
+            // 
+            // txtAddWord
+            // 
+            this.txtAddWord.Location = new System.Drawing.Point(148, 66);
+            this.txtAddWord.Name = "txtAddWord";
+            this.txtAddWord.Size = new System.Drawing.Size(342, 34);
+            this.txtAddWord.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 292);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 32);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Picture";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 125);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 32);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Meaning";
+            // 
+            // btnAddBrowsePicture
+            // 
+            this.btnAddBrowsePicture.Location = new System.Drawing.Point(425, 292);
+            this.btnAddBrowsePicture.Name = "btnAddBrowsePicture";
+            this.btnAddBrowsePicture.Size = new System.Drawing.Size(70, 34);
+            this.btnAddBrowsePicture.TabIndex = 1;
+            this.btnAddBrowsePicture.Text = "...";
+            this.btnAddBrowsePicture.UseVisualStyleBackColor = true;
+            this.btnAddBrowsePicture.Click += new System.EventHandler(this.btnAddBrowsePicture_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(338, 365);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(157, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Cooper Std Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(85, 148);
+            this.lbStatus.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(175, 32);
+            this.lbStatus.TabIndex = 0;
+            this.lbStatus.Text = "Status text";
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(883, 142);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(157, 45);
+            this.btnSaveData.TabIndex = 1;
+            this.btnSaveData.Text = "Save";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // btnResetDefault
+            // 
+            this.btnResetDefault.Location = new System.Drawing.Point(1060, 142);
+            this.btnResetDefault.Name = "btnResetDefault";
+            this.btnResetDefault.Size = new System.Drawing.Size(212, 45);
+            this.btnResetDefault.TabIndex = 1;
+            this.btnResetDefault.Text = "Reload data";
+            this.btnResetDefault.UseVisualStyleBackColor = true;
+            this.btnResetDefault.Click += new System.EventHandler(this.btnResetDefault_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1494, 748);
+            this.Controls.Add(this.btnResetDefault);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.lbTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Simple dictionary";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMeaning)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbMeaning;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.ListBox lsbWord;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbAddMeaning;
+        private System.Windows.Forms.TextBox txtAddWord;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btnSaveData;
+        private System.Windows.Forms.TextBox txtAddPicturePath;
+        private System.Windows.Forms.Button btnAddBrowsePicture;
+        private System.Windows.Forms.Button btnResetDefault;
+        private System.Windows.Forms.PictureBox ptbMeaning;
+    }
+}
+
---
+++ SimpleDictionary/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleDictionary
+{
+    public partial class Form1 : Form
+    {
+        private bool isHasDataFile, isHasImageFolder, isJustCreateFile = false;
+        private String currentPath;
+        private String dataFileName = "\\data.txt";
+        private String imageFolder = "\\images";
+
+        private String statusMessage;
+        private Dictionary<String, String> dict;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initApplication();
+            readData();
+        }
+
+        public void initApplication()
+        {
+            // Check enough file and folder, if not have, create it
+            // File: config.txt, data.txt, log.txt
+
+            // Get current path
+            currentPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+            // Check have data.txt file
+            isHasDataFile = File.Exists(currentPath + dataFileName);
+            isHasImageFolder = Directory.Exists(currentPath + imageFolder);
+
+            // Create file if not exist
+            if (!isHasDataFile)
+            {
+                File.Create(currentPath + dataFileName);
+                isJustCreateFile = true;
+            }
+
+            // Create image folder if not exist
+            if (!isHasImageFolder)
+            {
+                Directory.CreateDirectory(currentPath + imageFolder);
+            }
+
+            // Init dictionary
+            dict = new Dictionary<string, string>();
+
+            statusMessage = "Program init successfully";
+            setStatusMessage(statusMessage);
+        }
+
+        public void readData()
+        {
+            if (isJustCreateFile)
+            {
+                return;
+            }
+
+            String filePath = currentPath + dataFileName;
+            String[] data;
+            String word, meaning;
+            // Read data from file
+            using (FileStream fs = File.Open(filePath, FileMode.Open))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    using (StreamReader sr = new StreamReader(bs))
+                    {
+                        string str;
+                        while ((str = sr.ReadLine()) != null)
+                        {
+                            // Split data into dictionary
+                            // Data structure: word@meaning
+                            data = str.Split('@');
+                            word = data[0];
+                            meaning = data[1];
+                            if (!dict.ContainsKey(word))
+                            {
+                                dict.Add(word, meaning);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Set dataset for listbox
+            if(dict.Count > 0)
+            {
+                lsbWord.DataSource = new BindingSource(dict, null);
+                lsbWord.DisplayMember = "Key";
+                lsbWord.ValueMember = "Value";
+            }
+        }
+
+        public void writeData()
+        {
+            var filePath = currentPath + dataFileName;
+            // Delete old file
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            // Open file and write data to.
+            using (var sw = new StreamWriter(filePath, true))
+            {
+                foreach (var item in dict)
+                {
+                    // Get key & value from dict
+                    var key = item.Key;
+                    var value = item.Value;
+
+                    // Write to file
+                    sw.WriteLine(key + "@" + value);
+                }
+            }
+        }
+
+        public void translate()
+        {
+            // Get selected value value
+            String value = lsbWord.SelectedValue.ToString();
+
+            // Split into meaning and picture ("#")
+            String[] data = value.Split('#');
+
+            if (data != null && data.Length == 2)
+            {
+                // Fill to textarea
+                rtbMeaning.Text = data[0];
+
+                // Set picture
+                if (File.Exists(currentPath + imageFolder + "\\" + data[1]))
+                {
+                    ptbMeaning.Image = Image.FromFile(currentPath + imageFolder + "\\" + data[1]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            translate();
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            writeData();
+            // Set status message
+            setStatusMessage("Save success!");
+            MessageBox.Show("Save data success");
+
+        }
+
+        private void btnAddBrowsePicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;...";
+            dialog.InitialDirectory = @"C:\";
+            dialog.Title = "Please select an image file for the new word.";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set file path
+                txtAddPicturePath.Text = dialog.FileName;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Check data valid (has word, has meaning, has file in path)
+            bool isValid = false;
+            String word, meaning, picture;
+
+            word = txtAddWord.Text;
+            meaning = rtbAddMeaning.Text;
+            
+            if(!String.IsNullOrEmpty(word) && !String.IsNullOrEmpty(meaning))
+            {
+                isValid = true;
+            }
+
+            if (File.Exists(txtAddPicturePath.Text))
+            {
+                isValid = true;
+            }
+
+            if (!isValid)
+            {
+                MessageBox.Show("Data is not valid!");
+                return;
+            }
+
+            // Coppy image to folder
+            picture = System.IO.Path.GetFileName(txtAddPicturePath.Text);
+            var imgFolder = currentPath + imageFolder + "\\";
+            System.IO.File.Copy(txtAddPicturePath.Text, imgFolder + picture, true);
+
+            // Add to dict
+           
+            String line = word + "@" + meaning + "#" + picture;
+            if (dict.ContainsKey(word))
+            {
+                dict.Remove(word);
+            }
+
+
+            dict.Add(word, meaning + "#" + picture);
+
+            // Call write data
+            writeData();
+
+            MessageBox.Show("Add word success!!");
+        }
+
+        private void btnResetDefault_Click(object sender, EventArgs e)
+        {
+            initApplication();
+            readData();
+        }
+
+        public void setStatusMessage(String message)
+        {
+            lbStatus.Text = message;
+        }
+    }
+}
